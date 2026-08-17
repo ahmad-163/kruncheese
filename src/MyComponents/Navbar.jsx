9d753a1b8999a25fce7ac7ce72ba95ffd04a77c7
ahmad-163 <l1s23bscs0180@ucp.edu.pk>
2026-08-17 20:00:02 +0500

    comments

--- src/MyComponents/Navbar.jsx
+++ src/MyComponents/Navbar.jsx
@@ -194,7 +194,7 @@ function Navbar() {
                     <img 
                         src="/kruncheeselogo(2).png" 
                         alt="KRUNCHEESE" 
-                        style={{ height: isMobile ? '23px' : '30px' }}
+                        style={{ height: isMobile ? '15px' : '30px' }}
                         onError={(e) => {
                             e.target.style.display = 'none'
                             e.target.nextSibling.style.display = 'block'
